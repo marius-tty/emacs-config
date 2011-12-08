9937f3a8b49a31051740ba194b5b73895c0a0ee4
Marius Olsthoorn <marius@tty.nl>
2011-12-08 13:50:04 +0100

    Improved perl stuff

--- emacs.d/elisp/perl.el
+++ emacs.d/elisp/perl.el
@@ -1,28 +1,35 @@
 ;; Perl settings & tools
-
-(setq-default cperl-indent-level 4)
-(setq-default cperl-break-one-line-blocks-when-indent nil)
-(setq-default cperl-clobber-lisp-bindings t)
-(setq-default cperl-close-paren-offset -4)
-(setq-default cperl-electric-linefeed t)
-(setq-default cperl-font-lock t)
-(setq-default cperl-hairy nil)
-(setq-default cperl-indent-parens-as-block t)
-(setq-default cperl-indent-wrt-brace nil)
-(setq-default cperl-lazy-help-time 2)
-(setq-default cperl-lineup-step 4)
-(setq-default cperl-under-as-char t)
-(setq-default cperl-electric-parens nil)
-(setq cperl-auto-newline nil)
-
-(setq-default perl-indent-level 4)
+(eval-after-load "cperl-mode"
+  '(progn
+     (substitute-key-definition 'cperl-indent-command 'cperl-indent-line-or-region cperl-mode-map)
+     (setq-default cperl-indent-level 4)
+     (setq-default cperl-break-one-line-blocks-when-indent nil)
+     (setq-default cperl-clobber-lisp-bindings t)
+     (setq-default cperl-close-paren-offset -4)
+     (setq-default cperl-electric-linefeed t)
+     (setq-default cperl-font-lock t)
+     (setq-default cperl-hairy nil)
+     (setq-default cperl-indent-parens-as-block t)
+     (setq-default cperl-indent-wrt-brace nil)
+     (setq-default cperl-lazy-help-time 2)
+     (setq-default cperl-lineup-step 4)
+     (setq-default cperl-under-as-char t)
+     (setq-default cperl-electric-parens nil)
+     (setq cperl-auto-newline nil)
+     (setq-default perl-indent-level 4)))
 
 (defalias 'perl-mode 'cperl-mode)
 
 (defun perl-sub-list () "Display links to all sub's in a buffer."
   (interactive)
-  (list-matching-lines "^[[:blank:]]*sub[[:blank:]]")
-)
+  (list-matching-lines "^[[:blank:]]*sub[[:blank:]]"))
+
+
+(defun cperl-indent-line-or-region (&optional arg) "If a region is marked, indent region in CPerl mode, else indent current line as Perl code."
+  (interactive "P")
+  (if (and (not arg) mark-active)
+    (cperl-indent-region (mark) (point))
+    (cperl-indent-command (prefix-numeric-value arg))))
 
 (provide 'perl)
 
