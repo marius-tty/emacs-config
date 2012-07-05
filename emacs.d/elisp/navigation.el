604813f2286c000eba271146f734079b1a9cc7d5
Marius Olsthoorn <marius@tty.nl>
2012-07-05 09:54:09 +0200

    Added windmove

--- emacs.d/elisp/navigation.el
+++ emacs.d/elisp/navigation.el
@@ -6,5 +6,8 @@
 (ido-mode 1)
 (remove-hook 'minibuffer-setup-hook 'viper-minibuffer-setup-sentinel)
 
+;; Use alt-arrows to navigate between windows (alternative to C-x o)
+(windmove-default-keybindings 'meta)
+
 ;; end
 (provide 'navigation)
